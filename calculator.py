7eb96b4f1915802757f82cffa787c903e656d373
kevin-daroowalas <79782083+kevin-daroowalas@users.noreply.github.com>
2021-03-01 19:55:32 -0500

    Update calculator.py
    I executed the program and saw some flaws. 
    I corrected the flaws.
    when the (i) was 1 it did not increment like it was supposed too
    I had to convert the num into an integer because the for  loop cannot make a float value behave like an integer

--- calculator.py
+++ calculator.py
@@ -4,21 +4,21 @@ print('This program will imitate a calculator with 4 funtions')
 print(' ')
 print('The functions are addition - substraction - multiplication and division')
 print(' ')
-print('You can do a successive additon - substration - multiplication or division according to how many numbers you enter')
+print('You can do a successive additon - substration - multiplication or division according to how many number you enter')
 print(' ')
 print('You can also choose how many number you want to enter')
 print(' ')
 num = float(input('Please enter the number of values you would like to do the functions mentionned above :' ))
 list_num = []
-i=1
+i=0
 val=0
-for i in range (1,num):
-  if i==1:
+for i in range (0,int(num)):
+  if i==0:
     val=float(input('Please enter the first value :'))
     list_num.append(val)
     i+=1
   else:
     val=float(input('Please enter the next value : '))
-    print('this was the ',i,' value')
+    print('this was the ',i+1,' value')
     list_num.append(val)
     i+=1
